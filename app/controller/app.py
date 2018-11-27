1153e0e0c97b97d1c8c920e2e884f5765b556dd4
John-Michael O'Brien <JohnMichael.OBrien@colorado.edu>
2018-11-27 11:55:08 -0700

    Documentation!

--- app/controller/app.py
+++ app/controller/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 # John-Michael O'Brien
-# 09/24/2018
+# 11/21/2018
 #
-# A basic web service.
+# Provides a dashboard page and update API for pi solver jobs
+
 
 from flask import Flask
 from flask import request
@@ -13,47 +14,62 @@ import redis
 from decimal import *
 
 app = Flask(__name__)
+# Open a connection to the database
 r = redis.Redis(host='pi-solver-redis-service', port=6379, db=0)
 
+# Return static content directly
 @app.route('/static/<path:path>')
 def send_static(path):
     return send_from_directory('static', path)
 
+# Get the work results from the solver and update the database
 @app.route("/submitwork", methods=['POST'])
 def submit():
+    # Cache the data from the web request
     this_hits = int(request.form['hits'])
     this_throws = int(request.form['throws'])
 
+    # Create a single transaction and update all of the database values atomically.
     transact = r.pipeline(transaction=True)
     transact.incr("hits",this_hits);
     transact.incr("throws",this_throws);
     transact.incr("cycles",1);
     transact.execute();
 
+    # Send back a basic response
     return "Got request:\nHits: {0}\nThrows: {1}\n".format(this_hits, this_throws)
 
 @app.route("/")
 def root():
+    # Make an atomic request to the server for the current state
     results = r.mget(["hits","throws","cycles"])
+    # Cache the results
     local_hits = results[0]
     local_throws = results[1]
     local_cycles = results[2]
-	
+    
+    # If we didn't get anything
     if (not local_throws):
+        # Replace everything with nulls.
         local_throws=0
         local_hits=0
         local_cycles=0
         pi_est=0
     else:
+        # Otherwise, turn our results into bigints.
         local_throws=int(local_throws)
         local_hits=int(local_hits)
         local_cycles=int(local_cycles)
 
-	
+    # If we have actual data
     if (local_throws > 0):
+        # Calculate Pi as a Decimal.
         pi_est = Decimal(local_hits) / Decimal(local_throws) * Decimal(4.0)
     else:
+        # Otherwise say Pi is 0.
         pi_est = 0
-
+    
+    # return our rendered dashboard page. This should be seperated in the long term
+    # and AJAX api calls made instead, but this works for now.
     return render_template("index.html.tmpl",throws=local_throws,hits=local_hits,cycles=local_cycles,pi_est=pi_est);
 
--- app/solver/app.py
+++ app/solver/app.py
@@ -1,14 +1,25 @@
+#!/usr/bin/python
+
+# John-Michael O'Brien
+# 11/21/2018
+#
+# Throws darts at quadrant 1 of a dart board and reports the results
+# to an upstream API server via service discovery.
+
 import requests
 import random
 
 print("Starting tosses...")
 
+# Set the number of throws to make per report
 tests = 10000
 
+# Start throwing
 while (True):
     throws = 0
     hits = 0
 
+	# for each test we plan to do
     for i in range(0,tests):
         # Throw a dart
         x=random.SystemRandom().random()
@@ -20,8 +31,10 @@ while (True):
             # Mark the hit
             hits += 1
 
+	# Build up the report
     payload = {"hits":hits, "throws":throws}
 
+	# Make the report to the server.
     print("Threw {0} darts. Hit {1} times.".format(throws,hits))
     print("Sending request...")
     r = requests.post("http://pi-solver-controller-service/submitwork",data=payload)
